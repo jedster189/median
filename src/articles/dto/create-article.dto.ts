9a6d251fa197263e83fd646ab2c5c35342f8976d
Jared Egbert <jared.egbert@outlook.com>
2023-11-06 23:25:08 -0700

    Part 2

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,15 +1,36 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+  maxLength,
+  minLength,
+} from 'class-validator';
 
 export class CreateArticleDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
   @ApiProperty()
   title: string;
 
+  @IsString()
+  @IsOptional()
+  @IsNotEmpty()
+  @MaxLength(300)
   @ApiProperty({ required: false })
   description?: string;
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   body: string;
 
+  @IsBoolean()
+  @IsOptional()
   @ApiProperty({ required: false, default: false })
   published?: boolean = false;
   
